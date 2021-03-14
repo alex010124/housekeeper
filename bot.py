baf0325cbf4a701238fed06fffef0916bf892a17
alex010124 <uus010124@gmail.com>
2021-03-15 03:53:24 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,4 +1,22 @@
 import discord
-from discord import commands
+from discord.ext import commands
 
-bot = commands.bot(command-prefix="[")
+bot = commands.Bot(command_prefix="[")
+
+@bot.event
+async def on_ready():
+    print(">> Bot is online <<")
+
+@bot.event
+async def on_member_join(member):
+    print(f'{member} join!')
+    channel = bot.get_channel('820742681507659776')
+    await channel.send(f'{member} join!')
+
+@bot.event
+async def on_member_remove(member):
+    print(f'{member} leave!')
+    channel = bot.get_channel('820742719012995102')
+    await channel.send(f'{member} leave!')
+
+bot.run('ODE3NjU5Njg0OTk4NjEwOTg2.YEMu7A.bSvN2o_mKVow0mOVTPiDnMKaKzI')
