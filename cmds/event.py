95ad3ef3e14b6e295ef3a42c3ba54f0d2ea0ca36
alex010124 <uus010124@gmail.com>
2021-03-18 23:35:29 +0800

    Create event.py

---
+++ cmds/event.py
@@ -0,0 +1,21 @@
+import discord
+from discord.ext import commands
+from core.classes import Cog_Extension
+import json
+
+with open('setting.json' , mode = 'r' , encoding = 'utf8') as jfile:
+    jdata = json.load(jfile)
+
+class Event(Cog_Extension):
+    @commands.Cog.listener()
+    async def on_member_remove(self, member):
+        channel = self.bot.get_channel(int(jdata['LEAVE_CHANNEL']))
+        await channel.send(f'{member} leave!')
+        
+    @commands.Cog.listener()
+    async def on_message(self, msg):
+        if msg.content == 'apple' and msg.author != self.bot.user:
+            await msg.channel.send('apple')
+
+def setup(bot):
+    bot.add_cog(Event(bot))
