d8d6c97bf93f2a5835a24ee8bb0f2a123f81baee
alex010124 <uus010124@gmail.com>
2021-03-18 23:35:39 +0800

    Create task.py

---
+++ cmds/task.py
@@ -0,0 +1,24 @@
+import discord
+from discord.ext import commands
+from core.classes import Cog_Extension
+import json , asyncio , datetime
+
+class Task(Cog_Extension):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        async def interval():
+            await selt.bot.wait_until_ready()
+            self.channel = self.bot.get_channel('820742681507659776')
+            while not self.bot.is_closed():
+                await self.channel.send("hi")
+                await asyncio.sleep(5)
+            self.bg_task = self.bot.loop.create_task(interval())    
+
+
+
+
+
+
+def setup(bot):
+    bot.add_cog(Task(bot))
