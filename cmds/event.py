549bd9e77b3a852059b73d0251115e268376c1bc
alex010124 <uus010124@gmail.com>
2021-03-19 14:50:58 +0800

    Update event.py

--- cmds/event.py
+++ cmds/event.py
@@ -17,5 +17,36 @@ class Event(Cog_Extension):
         if msg.content == 'apple' and msg.author != self.bot.user:
             await msg.channel.send('apple')
 
+    @commands.Cog.listener()
+    async def on_raw_reaction_add(self, payload):
+        print(payload.emoji)
+        if payload.message_id == 822355740181790790:
+            if str(payload.emoji) == '<:cha:814150233784451143>':
+                guild = self.bot.get_guild(payload.guild_id)
+                role = guild.get_role(691293603120807949)
+                await payload.member.add_roles(role)
+                await payload.member.send(f"you get {role}")
+            elif str(payload.emoji) == '<:leon:695249835045027913>':
+                guild = self.bot.get_guild(payload.guild_id)
+                role = guild.get_role(787619351326883840)
+                await payload.member.add_roles(role)
+                await payload.member.send(f"you get {role}")
+
+    @commands.Cog.listener()
+    async def on_raw_reaction_remove(self, payload):
+        if payload.message_id == 822355740181790790:
+            if str(payload.emoji) == '<:cha:814150233784451143>':
+                guild = self.bot.get_guild(payload.guild_id)
+                user = await guild.fetch_member(payload.user_id)
+                role = guild.get_role(691293603120807949)
+                await user.remove_roles(role)   
+                await user.send(f"you remove{role}")
+            elif str(payload.emoji) == '<:leon:695249835045027913>':
+                guild = self.bot.get_guild(payload.guild_id)
+                user = await guild.fetch_member(payload.user_id)
+                role = guild.get_role(787619351326883840)
+                await user.remove_roles(role)   
+                await user.send(f"you remove{role}")
+
 def setup(bot):
     bot.add_cog(Event(bot))
